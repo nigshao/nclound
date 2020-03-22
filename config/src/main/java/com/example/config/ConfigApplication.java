7dfdb50849289481e5802ae15df4984cbac46410
Ningshaobo <ningshaobo@zhkgr.com>
2020-03-22 20:18:09 +0800

    sdfjasj

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -2,9 +2,8 @@ package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+//@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
