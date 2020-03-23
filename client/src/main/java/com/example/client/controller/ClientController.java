bfff94460481ea245cbbdcb66835c4febb436a27
Ningshaobo <ningshaobo@zhkgr.com>
2020-03-23 17:35:00 +0800

    养成

--- client/src/main/java/com/example/client/controller/ClientController.java
+++ client/src/main/java/com/example/client/controller/ClientController.java
@@ -3,7 +3,7 @@ package com.example.client.controller;
 import com.example.client.service.FeignSrvice;
 import com.example.common.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +15,59 @@ import java.net.URL;
 @RequestMapping("/client")
 public class ClientController {
 
-    private static  final String  URL  = "http://SERVER-POWER";
+    private static final String URL = "http://SERVER-POWER";
 
     @Autowired
-    RestTemplate  restTemplate;
-
-
+    RestTemplate restTemplate;
 
 
     @Autowired
-    FeignSrvice  feignSrvice;
+    FeignSrvice feignSrvice;
+
+    @Value("${testUser.name}")
+    private String name;
+
+
+    @Value("${testUser.age}")
+    private Integer age;
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
 
     //rabbion  调用
     @RequestMapping("/power.do")
-    private Object  getPower(){
-      // return restTemplate.getForObject(URL+"/power.do",Object.class);
-      return feignSrvice.getPower();
+    private Object getPower() {
+        // return restTemplate.getForObject(URL+"/power.do",Object.class);
+        return feignSrvice.getPower();
     }
+
     //feigon 调用
     @GetMapping("/updateUser")
-    private Object  updateUser(){
-        User  user  = new User();
+    private Object updateUser() {
+        User user = new User();
         user.setAge(12);
-        return  feignSrvice.updateUser(user);
+        return feignSrvice.updateUser(user);
     }
+
+    @GetMapping("/testName.do")
+    private Object testName() {
+        return "name: " + name + "age: " + age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
