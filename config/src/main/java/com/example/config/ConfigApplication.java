729a573671feb7cb7ba1b4b96bbbd2d72c9335c6
Ningshaobo <ningshaobo@zhkgr.com>
2020-03-23 14:43:21 +0800

    sdfjasj

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -2,8 +2,12 @@ package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableConfigServer
+
+@EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
